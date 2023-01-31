00638b75b808e53fcfa4d733fd703304d19afb24
Fast <faustogarcia1357@gmail.com>
2023-01-30 23:15:27 -0300

    facu revisionm xd lol uwu

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,34 +1,52 @@
 import './Task.css'
-import { NewTask} from '../NewTask/NewTask'
-import { useState } from 'react';
-const Task = ()=>{
-    let [input, setInput] = useState('');
-    let [tasks, setTasks] = useState([]);
-    function createTask(){
-        if (input !== ''){
-            setTasks([...tasks, input]);
-            setInput('');
-        }
+import { NewTask } from '../NewTask/NewTask'
+import { useState, useContext } from 'react'
+import { ThemeContext } from '../../Context/ThemeContext'
+import { useLocalStorage } from '../hooks/useLocalStorage'
+
+const Task = () => {
+  const { darkMode, setDarkMode, darkTheme, lightTheme } = useContext(ThemeContext)
+
+  const [input, setInput] = useState('')
+  const [tasks, setTasks] = useLocalStorage('tasks', [])
+
+  function createTask () {
+    if (input !== '') {
+      setTasks([...tasks, input])
+      setInput('')
     }
-  
+  }
+  function toggleMode () {
+    if (darkMode) {
+      setDarkMode(false)
+    } else {
+      setDarkMode(true)
+    }
+  }
 
-    return(
-        <main className="Task">
-            <section className='Task__section-dashboard'>
-                <input className='Task__input' value={input} onChange={event=>{
-                    setInput(event.target.value);
-                }}/>
-                <button className='Task__button-create' onClick={createTask}>Crear</button>
-            </section>
-                
-            <section className='Task__section-task'>
-                <ul className='Task__ul'>
-                    <NewTask lista={tasks} setLista={setTasks}/>
-                </ul>
-            </section>
+  return (
+        <>
+            <header className='header' style={{ backgroundColor: darkMode ? darkTheme.headerBackground : lightTheme.headerBackground }}>
+                <h1 className='header__h1'style={{ color: darkMode ? darkTheme.text : lightTheme.text }}>{darkMode ? darkTheme.mode : lightTheme.mode} </h1>
+                <button className='header__button' onClick={toggleMode} style={{ backgroundColor: darkMode ? darkTheme.backgroundButton : lightTheme.backgroundButton }} ></button>
+            </header>
+            <main className="Task"style={{ backgroundColor: darkMode ? darkTheme.background : lightTheme.background }}>
+                <section className='Task__section-dashboard'>
+                    <input className='Task__input' value={input} style={{ backgroundColor: darkMode ? darkTheme.input : lightTheme.input, border: 'none', color: darkMode ? darkTheme.text : lightTheme.text }}onChange={event => {
+                      setInput(event.target.value)
+                    }}/>
+                    <button className='Task__button-create' onClick={(event) => { createTask() }} style={{ backgroundColor: darkMode ? darkTheme.input : lightTheme.input, border: 'none', color: darkMode ? darkTheme.text : lightTheme.text }}>Crear</button>
+                </section>
 
-        </main>
-        
-    )
+                <section className='Task__section-task'>
+                    <ul className='Task__ul'>
+                        <NewTask lista={tasks} setLista={setTasks}/>
+                    </ul>
+                </section>
+
+            </main>
+        </>
+
+  )
 }
 export default Task
